4422178485fe8eb7c549dba5e123bc8f4f232acc
Klamix <110839852+Klamix@users.noreply.github.com>
2026-07-28 18:27:35 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// 1. Находим нашу кнопку в документе по её id
-const button = document.querySelectorAll(".menu-btn");
-
-button.forEach(button => {
-    button.addEventListener('click', () => {
-        window.location.href = button.getAttribute('data-url');
-    })
-})
