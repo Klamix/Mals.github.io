0da1ecf4ccac77a4041fb2e2b30e0de9ee464197
Klamix <110839852+Klamix@users.noreply.github.com>
2026-07-28 18:28:27 +0500

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,28 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+const button = document.getElementById("mybutton")
+const reg = document.getElementById("register")
+
+const inputs = reg.querySelectorAll('input');
+const name_imp = inputs[0];
+const pass_imp = inputs[1];
+
+const friends = document.getElementById('Friends')
+
+
+
+
+
+
+button.addEventListener('click', function () {
+
+    const name = name_imp.value;
+    const pass = pass_imp.value;
+    if (name == "") {
+        reg.classList.add("closed");
+        setTimeout(() => reg.remove(), 1400);
+        friends.classList.add('visible')
+    }
+});
